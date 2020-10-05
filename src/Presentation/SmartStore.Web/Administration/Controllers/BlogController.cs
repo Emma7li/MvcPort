6c98d467dd515d7793c97bf152b286fb7431aacf
Emma7li <www.ema7li@gmail.com>
2020-10-05 17:57:13 +0300

    Added initial files to start

---
+++ src/Presentation/SmartStore.Web/Administration/Controllers/BlogController.cs
@@ -0,0 +1,373 @@
+﻿using System;
+using System.Linq;
+using System.Web.Mvc;
+using SmartStore.Admin.Models.Blogs;
+using SmartStore.Core;
+using SmartStore.Core.Domain.Blogs;
+using SmartStore.Core.Domain.Common;
+using SmartStore.Core.Html;
+using SmartStore.Core.Security;
+using SmartStore.Services.Blogs;
+using SmartStore.Services.Customers;
+using SmartStore.Services.Helpers;
+using SmartStore.Services.Localization;
+using SmartStore.Services.Seo;
+using SmartStore.Services.Stores;
+using SmartStore.Web.Framework.Controllers;
+using SmartStore.Web.Framework.Filters;
+using SmartStore.Web.Framework.Modelling;
+using SmartStore.Web.Framework.Security;
+using Telerik.Web.Mvc;
+
+namespace SmartStore.Admin.Controllers
+{
+    [AdminAuthorize]
+    public class BlogController : AdminControllerBase
+    {
+        #region Fields
+
+        private readonly IBlogService _blogService;
+        private readonly ILanguageService _languageService;
+        private readonly IDateTimeHelper _dateTimeHelper;
+        private readonly ICustomerContentService _customerContentService;
+        private readonly ILocalizationService _localizationService;
+        private readonly IUrlRecordService _urlRecordService;
+        private readonly IStoreService _storeService;
+        private readonly IStoreMappingService _storeMappingService;
+        private readonly ICustomerService _customerService;
+        private readonly AdminAreaSettings _adminAreaSettings;
+
+        #endregion
+
+        #region Constructors
+
+        public BlogController(
+            IBlogService blogService,
+            ILanguageService languageService,
+            IDateTimeHelper dateTimeHelper,
+            ICustomerContentService customerContentService,
+            ILocalizationService localizationService,
+            IUrlRecordService urlRecordService,
+            IStoreService storeService,
+            IStoreMappingService storeMappingService,
+            ICustomerService customerService,
+            AdminAreaSettings adminAreaSettings)
+        {
+            _blogService = blogService;
+            _languageService = languageService;
+            _dateTimeHelper = dateTimeHelper;
+            _customerContentService = customerContentService;
+            _localizationService = localizationService;
+            _urlRecordService = urlRecordService;
+            _storeService = storeService;
+            _storeMappingService = storeMappingService;
+            _customerService = customerService;
+            _adminAreaSettings = adminAreaSettings;
+        }
+
+        #endregion
+
+        #region Utilities
+
+        [NonAction]
+        private void PrepareStoresMappingModel(BlogPostModel model, BlogPost blogPost, bool excludeProperties)
+        {
+            Guard.NotNull(model, nameof(model));
+
+            if (!excludeProperties)
+            {
+                model.SelectedStoreIds = _storeMappingService.GetStoresIdsWithAccess(blogPost);
+            }
+        }
+
+        #endregion
+
+        #region Blog posts
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        [Permission(Permissions.Cms.Blog.Read)]
+        public ActionResult List()
+        {
+            ViewBag.GridPageSize = _adminAreaSettings.GridPageSize;
+            ViewBag.AllLanguages = _languageService.GetAllLanguages(true);
+
+            // PrepareSearchModel
+            var model = new BlogListModel();
+
+            // Tags
+            var allTags = _blogService.GetAllBlogPostTags(0, 0, true).Select(x => x.Name).ToList();
+            model.SearchAvailableTags = new MultiSelectList(allTags);
+
+            // IsSingleStoreMode & IsMultiLangMode
+            model.IsSingleStoreMode = _storeService.IsSingleStoreMode();
+            model.IsSingleLangMode = _languageService.GetAllLanguages(true).Count == 1;
+
+            // Set end date to now.
+            model.SearchEndDate = DateTime.UtcNow;
+
+            return View(model);
+        }
+
+        [HttpPost, GridAction(EnableCustomBinding = true)]
+        [Permission(Permissions.Cms.Blog.Read)]
+        public ActionResult List(GridCommand command, BlogListModel model)
+        {
+            var blogPosts = _blogService.GetAllBlogPosts(model.SearchStoreId, model.SearchLanguageId, model.SearchStartDate, model.SearchEndDate, command.Page - 1, command.PageSize,
+                !model.SearchIsPublished ?? true,
+                title: model.SearchTitle, intro: model.SearchIntro, body: model.SearchBody, tag: model.SearchTags);
+
+            var gridModel = new GridModel<BlogPostModel>
+            {
+                Total = blogPosts.TotalCount
+            };
+
+            gridModel.Data = blogPosts.Select(x =>
+            {
+                var m = x.ToModel();
+
+                if (x.StartDateUtc.HasValue)
+                {
+                    m.StartDate = _dateTimeHelper.ConvertToUserTime(x.StartDateUtc.Value, DateTimeKind.Utc);
+                }
+                if (x.EndDateUtc.HasValue)
+                {
+                    m.EndDate = _dateTimeHelper.ConvertToUserTime(x.EndDateUtc.Value, DateTimeKind.Utc);
+                }
+
+                m.CreatedOn = _dateTimeHelper.ConvertToUserTime(x.CreatedOnUtc, DateTimeKind.Utc);
+                m.LanguageName = x.Language.Name;
+                m.Comments = x.ApprovedCommentCount + x.NotApprovedCommentCount;
+
+                return m;
+            });
+
+            return new JsonResult
+            {
+                Data = gridModel
+            };
+        }
+
+        [Permission(Permissions.Cms.Blog.Create)]
+        public ActionResult Create()
+        {
+            ViewBag.AllLanguages = _languageService.GetAllLanguages(true);
+
+            var model = new BlogPostModel
+            {
+                CreatedOnUtc = DateTime.UtcNow,
+                AllowComments = true
+            };
+
+            var allTags = _blogService.GetAllBlogPostTags(0, 0, true).Select(x => x.Name).ToList();
+            model.AvailableTags = new MultiSelectList(allTags, model.AvailableTags);
+
+            PrepareStoresMappingModel(model, null, false);
+
+            return View(model);
+        }
+
+        [HttpPost, ValidateInput(false), ParameterBasedOnFormName("save-continue", "continueEditing")]
+        [Permission(Permissions.Cms.Blog.Create)]
+        public ActionResult Create(BlogPostModel model, bool continueEditing, FormCollection form)
+        {
+            if (ModelState.IsValid)
+            {
+                var blogPost = model.ToEntity();
+
+                blogPost.CreatedOnUtc = model.CreatedOnUtc;
+                blogPost.StartDateUtc = model.StartDate;
+                blogPost.EndDateUtc = model.EndDate;
+
+                _blogService.InsertBlogPost(blogPost);
+
+                // Search engine name.
+                var seName = blogPost.ValidateSeName(model.SeName, model.Title, true);
+                _urlRecordService.SaveSlug(blogPost, seName, blogPost.LanguageId);
+
+                SaveStoreMappings(blogPost, model.SelectedStoreIds);
+
+                Services.EventPublisher.Publish(new ModelBoundEvent(model, blogPost, form));
+
+                NotifySuccess(T("Admin.ContentManagement.Blog.BlogPosts.Added"));
+                return continueEditing ? RedirectToAction("Edit", new { id = blogPost.Id }) : RedirectToAction("List");
+            }
+
+            ViewBag.AllLanguages = _languageService.GetAllLanguages(true);
+
+            // Tags
+            var allTags = _blogService.GetAllBlogPostTags(0, 0, true).Select(x => x.Name).ToList();
+            model.AvailableTags = new MultiSelectList(allTags, model.AvailableTags);
+
+            PrepareStoresMappingModel(model, null, true);
+
+            return View(model);
+        }
+
+        [Permission(Permissions.Cms.Blog.Read)]
+        public ActionResult Edit(int id)
+        {
+            var blogPost = _blogService.GetBlogPostById(id);
+            if (blogPost == null)
+                return RedirectToAction("List");
+
+            ViewBag.AllLanguages = _languageService.GetAllLanguages(true);
+
+            var model = blogPost.ToModel();
+            model.CreatedOnUtc = blogPost.CreatedOnUtc;
+            model.StartDate = blogPost.StartDateUtc;
+            model.EndDate = blogPost.EndDateUtc;
+
+            // Tags
+            var allTags = _blogService.GetAllBlogPostTags(0, 0, true).Select(x => x.Name).ToList();
+            model.AvailableTags = new MultiSelectList(allTags, model.AvailableTags);
+            model.Tags = blogPost.Tags.SplitSafe(",");
+
+            // Store
+            PrepareStoresMappingModel(model, blogPost, false);
+            return View(model);
+        }
+
+        [HttpPost, ValidateInput(false), ParameterBasedOnFormName("save-continue", "continueEditing")]
+        [Permission(Permissions.Cms.Blog.Update)]
+        public ActionResult Edit(BlogPostModel model, bool continueEditing, FormCollection form)
+        {
+            var blogPost = _blogService.GetBlogPostById(model.Id);
+            if (blogPost == null)
+                return RedirectToAction("List");
+
+            if (ModelState.IsValid)
+            {
+                blogPost = model.ToEntity(blogPost);
+
+                blogPost.CreatedOnUtc = model.CreatedOnUtc;
+                blogPost.StartDateUtc = model.StartDate;
+                blogPost.EndDateUtc = model.EndDate;
+
+                _blogService.UpdateBlogPost(blogPost);
+
+                // Search engine name.
+                var seName = blogPost.ValidateSeName(model.SeName, model.Title, true);
+                _urlRecordService.SaveSlug(blogPost, seName, blogPost.LanguageId);
+
+                SaveStoreMappings(blogPost, model.SelectedStoreIds);
+
+                Services.EventPublisher.Publish(new ModelBoundEvent(model, blogPost, form));
+                NotifySuccess(T("Admin.ContentManagement.Blog.BlogPosts.Updated"));
+
+                return continueEditing ? RedirectToAction("Edit", new { id = blogPost.Id }) : RedirectToAction("List");
+            }
+
+            ViewBag.AllLanguages = _languageService.GetAllLanguages(true);
+
+            // Tags
+            var allTags = _blogService.GetAllBlogPostTags(0, 0, true).Select(x => x.Name).ToList();
+            model.AvailableTags = new MultiSelectList(allTags, model.AvailableTags);
+            model.Tags = blogPost.Tags.SplitSafe(",");
+
+            PrepareStoresMappingModel(model, blogPost, true);
+
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [Permission(Permissions.Cms.Blog.Delete)]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var blogPost = _blogService.GetBlogPostById(id);
+            if (blogPost == null)
+                return RedirectToAction("List");
+
+            _blogService.DeleteBlogPost(blogPost);
+
+            NotifySuccess(_localizationService.GetResource("Admin.ContentManagement.Blog.BlogPosts.Deleted"));
+            return RedirectToAction("List");
+        }
+
+        #endregion
+
+        #region Comments
+
+        [Permission(Permissions.Cms.Blog.Read)]
+        public ActionResult Comments(int? filterByBlogPostId)
+        {
+            ViewBag.FilterByBlogPostId = filterByBlogPostId;
+            ViewBag.GridPageSize = _adminAreaSettings.GridPageSize;
+
+            return View();
+        }
+
+        [HttpPost, GridAction(EnableCustomBinding = true)]
+        [Permission(Permissions.Cms.Blog.Read)]
+        public ActionResult Comments(int? filterByBlogPostId, GridCommand command)
+        {
+            IPagedList<BlogComment> comments;
+
+            if (filterByBlogPostId.HasValue)
+            {
+                // Filter comments by blog.
+                var query = _customerContentService.GetAllCustomerContent<BlogComment>(0, null).SourceQuery;
+                query = query.Where(x => x.BlogPostId == filterByBlogPostId.Value);
+
+                comments = new PagedList<BlogComment>(query, command.Page - 1, command.PageSize);
+            }
+            else
+            {
+                // Load all blog comments.
+                comments = _customerContentService.GetAllCustomerContent<BlogComment>(0, null, null, null, command.Page - 1, command.PageSize);
+            }
+
+            var customerIds = comments.Select(x => x.CustomerId).Distinct().ToArray();
+            var customers = _customerService.GetCustomersByIds(customerIds).ToDictionarySafe(x => x.Id);
+
+            var model = new GridModel<BlogCommentModel>
+            {
+                Total = comments.TotalCount
+            };
+
+            model.Data = comments.Select(blogComment =>
+            {
+                customers.TryGetValue(blogComment.CustomerId, out var customer);
+
+                var commentModel = new BlogCommentModel
+                {
+                    Id = blogComment.Id,
+                    BlogPostId = blogComment.BlogPostId,
+                    BlogPostTitle = blogComment.BlogPost.Title,
+                    CustomerId = blogComment.CustomerId,
+                    IpAddress = blogComment.IpAddress,
+                    CreatedOn = _dateTimeHelper.ConvertToUserTime(blogComment.CreatedOnUtc, DateTimeKind.Utc),
+                    Comment = HtmlUtils.ConvertPlainTextToHtml(blogComment.CommentText.HtmlEncode()),
+                    CustomerName = customer.GetDisplayName(T)
+                };
+
+                return commentModel;
+            });
+
+            return new JsonResult
+            {
+                Data = model
+            };
+        }
+
+        [GridAction(EnableCustomBinding = true)]
+        [Permission(Permissions.Cms.Blog.EditComment)]
+        public ActionResult CommentDelete(int? filterByBlogPostId, int id, GridCommand command)
+        {
+            var comment = _customerContentService.GetCustomerContentById(id) as BlogComment;
+
+            var blogPost = comment.BlogPost;
+            _customerContentService.DeleteCustomerContent(comment);
+
+            //update totals
+            _blogService.UpdateCommentTotals(blogPost);
+
+            return Comments(filterByBlogPostId, command);
+        }
+
+        #endregion
+    }
+}
---
+++ src/Presentation/SmartStore.Web/Administration/Controllers/NewsController.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+using SmartStore.Admin.Models.News;
+using SmartStore.Core;
+using SmartStore.Core.Domain.Common;
+using SmartStore.Core.Domain.News;
+using SmartStore.Core.Html;
+using SmartStore.Core.Security;
+using SmartStore.Services.Customers;
+using SmartStore.Services.Helpers;
+using SmartStore.Services.Localization;
+using SmartStore.Services.News;
+using SmartStore.Services.Seo;
+using SmartStore.Services.Stores;
+using SmartStore.Web.Framework.Controllers;
+using SmartStore.Web.Framework.Filters;
+using SmartStore.Web.Framework.Modelling;
+using SmartStore.Web.Framework.Security;
+using Telerik.Web.Mvc;
+
+namespace SmartStore.Admin.Controllers
+{
+    [AdminAuthorize]
+    public class NewsController : AdminControllerBase
+    {
+        #region Fields
+
+        private readonly INewsService _newsService;
+        private readonly ILanguageService _languageService;
+        private readonly IDateTimeHelper _dateTimeHelper;
+        private readonly ICustomerContentService _customerContentService;
+        private readonly IUrlRecordService _urlRecordService;
+        private readonly IStoreService _storeService;
+        private readonly IStoreMappingService _storeMappingService;
+        private readonly ICustomerService _customerService;
+        private readonly AdminAreaSettings _adminAreaSettings;
+
+        #endregion
+
+        #region Constructors
+
+        public NewsController(
+            INewsService newsService,
+            ILanguageService languageService,
+            IDateTimeHelper dateTimeHelper,
+            ICustomerContentService customerContentService,
+            IUrlRecordService urlRecordService,
+            IStoreService storeService,
+            IStoreMappingService storeMappingService,
+            ICustomerService customerService,
+            AdminAreaSettings adminAreaSettings)
+        {
+            _newsService = newsService;
+            _languageService = languageService;
+            _dateTimeHelper = dateTimeHelper;
+            _customerContentService = customerContentService;
+            _urlRecordService = urlRecordService;
+            _storeService = storeService;
+            _storeMappingService = storeMappingService;
+            _customerService = customerService;
+            _adminAreaSettings = adminAreaSettings;
+        }
+
+        #endregion
+
+        #region Utilities
+
+        [NonAction]
+        private void PrepareStoresMappingModel(NewsItemModel model, NewsItem newsItem, bool excludeProperties)
+        {
+            Guard.NotNull(model, nameof(model));
+
+            if (!excludeProperties)
+            {
+                model.SelectedStoreIds = _storeMappingService.GetStoresIdsWithAccess(newsItem);
+            }
+        }
+
+        #endregion
+
+        #region News items
+
+        public ActionResult Index()
+        {
+            return RedirectToAction("List");
+        }
+
+        [Permission(Permissions.Cms.News.Read)]
+        public ActionResult List()
+        {
+            ViewBag.AllLanguages = _languageService.GetAllLanguages(true);
+
+            var model = new NewsItemListModel();
+
+            // IsSingleStoreMode & IsMultiLangMode
+            model.IsSingleStoreMode = _storeService.IsSingleStoreMode();
+            model.IsSingleLangMode = _languageService.GetAllLanguages(true).Count == 1;
+
+            // Set end date to now.
+            model.SearchEndDate = DateTime.UtcNow;
+
+            return View(model);
+        }
+
+        [HttpPost, GridAction(EnableCustomBinding = true)]
+        [Permission(Permissions.Cms.News.Read)]
+        public ActionResult List(GridCommand command, NewsItemListModel model)
+        {
+            var news = _newsService.GetAllNews(model.SearchLanguageId, model.SearchStoreId, command.Page - 1, command.PageSize, !model.SearchIsPublished ?? true,
+                title: model.SearchTitle, intro: model.SearchShort, full: model.SearchFull);
+
+            var gridModel = new GridModel<NewsItemModel>
+            {
+                Total = news.TotalCount
+            };
+
+            gridModel.Data = news.Select(x =>
+            {
+                var m = x.ToModel();
+
+                if (x.StartDateUtc.HasValue)
+                {
+                    m.StartDate = _dateTimeHelper.ConvertToUserTime(x.StartDateUtc.Value, DateTimeKind.Utc);
+                }
+                if (x.EndDateUtc.HasValue)
+                {
+                    m.EndDate = _dateTimeHelper.ConvertToUserTime(x.EndDateUtc.Value, DateTimeKind.Utc);
+                }
+
+                m.CreatedOn = _dateTimeHelper.ConvertToUserTime(x.CreatedOnUtc, DateTimeKind.Utc);
+                m.LanguageName = x.Language.Name;
+                m.Comments = x.ApprovedCommentCount + x.NotApprovedCommentCount;
+
+                return m;
+            });
+
+            return new JsonResult
+            {
+                Data = gridModel
+            };
+        }
+
+        [Permission(Permissions.Cms.News.Create)]
+        public ActionResult Create()
+        {
+            ViewBag.AllLanguages = _languageService.GetAllLanguages(true);
+
+            var model = new NewsItemModel
+            {
+                Published = true,
+                AllowComments = true
+            };
+
+            PrepareStoresMappingModel(model, null, false);
+
+            //default values
+            model.Published = true;
+            model.AllowComments = true;
+            model.CreatedOn = DateTime.UtcNow;
+
+            return View(model);
+        }
+
+        [HttpPost, ValidateInput(false), ParameterBasedOnFormName("save-continue", "continueEditing")]
+        [Permission(Permissions.Cms.News.Create)]
+        public ActionResult Create(NewsItemModel model, bool continueEditing, FormCollection form)
+        {
+            if (ModelState.IsValid)
+            {
+                var newsItem = model.ToEntity();
+
+                newsItem.StartDateUtc = model.StartDate;
+                newsItem.EndDateUtc = model.EndDate;
+                newsItem.CreatedOnUtc = model.CreatedOn;
+                _newsService.InsertNews(newsItem);
+
+                // Search engine name.
+                var seName = newsItem.ValidateSeName(model.SeName, model.Title, true);
+                _urlRecordService.SaveSlug(newsItem, seName, newsItem.LanguageId);
+
+                SaveStoreMappings(newsItem, model.SelectedStoreIds);
+
+                Services.EventPublisher.Publish(new ModelBoundEvent(model, newsItem, form));
+
+                NotifySuccess(T("Admin.ContentManagement.News.NewsItems.Added"));
+                return continueEditing ? RedirectToAction("Edit", new { id = newsItem.Id }) : RedirectToAction("List");
+            }
+
+            // If we got this far, something failed, redisplay form.
+            ViewBag.AllLanguages = _languageService.GetAllLanguages(true);
+
+            PrepareStoresMappingModel(model, null, true);
+
+            return View(model);
+        }
+
+        [Permission(Permissions.Cms.News.Read)]
+        public ActionResult Edit(int id)
+        {
+            var newsItem = _newsService.GetNewsById(id);
+            if (newsItem == null)
+                return RedirectToAction("List");
+
+            ViewBag.AllLanguages = _languageService.GetAllLanguages(true);
+
+            var model = newsItem.ToModel();
+            model.StartDate = newsItem.StartDateUtc;
+            model.EndDate = newsItem.EndDateUtc;
+            model.CreatedOn = newsItem.CreatedOnUtc;
+
+            PrepareStoresMappingModel(model, newsItem, false);
+
+            return View(model);
+        }
+
+        [HttpPost, ValidateInput(false), ParameterBasedOnFormName("save-continue", "continueEditing")]
+        [Permission(Permissions.Cms.News.Update)]
+        public ActionResult Edit(NewsItemModel model, bool continueEditing, FormCollection form)
+        {
+            var newsItem = _newsService.GetNewsById(model.Id);
+            if (newsItem == null)
+                return RedirectToAction("List");
+
+            if (ModelState.IsValid)
+            {
+                newsItem = model.ToEntity(newsItem);
+
+                newsItem.StartDateUtc = model.StartDate;
+                newsItem.EndDateUtc = model.EndDate;
+                newsItem.CreatedOnUtc = model.CreatedOn;
+                _newsService.UpdateNews(newsItem);
+
+                // Search engine name.
+                var seName = newsItem.ValidateSeName(model.SeName, model.Title, true);
+                _urlRecordService.SaveSlug(newsItem, seName, newsItem.LanguageId);
+
+                SaveStoreMappings(newsItem, model.SelectedStoreIds);
+
+                Services.EventPublisher.Publish(new ModelBoundEvent(model, newsItem, form));
+
+                NotifySuccess(T("Admin.ContentManagement.News.NewsItems.Updated"));
+                return continueEditing ? RedirectToAction("Edit", new { id = newsItem.Id }) : RedirectToAction("List");
+            }
+
+            // If we got this far, something failed, redisplay form.
+            ViewBag.AllLanguages = _languageService.GetAllLanguages(true);
+
+            PrepareStoresMappingModel(model, newsItem, true);
+
+            return View(model);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        [Permission(Permissions.Cms.News.Delete)]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var newsItem = _newsService.GetNewsById(id);
+            if (newsItem == null)
+                return RedirectToAction("List");
+
+            _newsService.DeleteNews(newsItem);
+
+            NotifySuccess(T("Admin.ContentManagement.News.NewsItems.Deleted"));
+            return RedirectToAction("List");
+        }
+
+        [HttpPost]
+        [Permission(Permissions.Cms.News.Delete)]
+        public ActionResult DeleteSelected(ICollection<int> selectedIds)
+        {
+            if (selectedIds != null)
+            {
+                var news = _newsService.GetNewsByIds(selectedIds.ToArray()).ToList();
+
+                news.ForEach(x => _newsService.DeleteNews(x));
+            }
+
+            return Json(new { Result = true });
+        }
+
+        #endregion
+
+        #region Comments
+
+        [Permission(Permissions.Cms.News.Read)]
+        public ActionResult Comments(int? filterByNewsItemId)
+        {
+            ViewBag.FilterByNewsItemId = filterByNewsItemId;
+            ViewBag.GridPageSize = _adminAreaSettings.GridPageSize;
+
+            return View();
+        }
+
+        [HttpPost, GridAction(EnableCustomBinding = true)]
+        [Permission(Permissions.Cms.News.Read)]
+        public ActionResult Comments(int? filterByNewsItemId, GridCommand command)
+        {
+            IPagedList<NewsComment> comments;
+
+            if (filterByNewsItemId.HasValue)
+            {
+                // Filter comments by news item.
+                var query = _customerContentService.GetAllCustomerContent<NewsComment>(0, null).SourceQuery;
+                query = query.Where(x => x.NewsItemId == filterByNewsItemId.Value);
+
+                comments = new PagedList<NewsComment>(query, command.Page - 1, command.PageSize);
+            }
+            else
+            {
+                // Load all news comments.
+                comments = _customerContentService.GetAllCustomerContent<NewsComment>(0, null, null, null, command.Page - 1, command.PageSize);
+            }
+
+            var customerIds = comments.Select(x => x.CustomerId).Distinct().ToArray();
+            var customers = _customerService.GetCustomersByIds(customerIds).ToDictionarySafe(x => x.Id);
+
+            var model = new GridModel<NewsCommentModel>
+            {
+                Total = comments.TotalCount
+            };
+
+            model.Data = comments.Select(newsComment =>
+            {
+                customers.TryGetValue(newsComment.CustomerId, out var customer);
+
+                var commentModel = new NewsCommentModel
+                {
+                    Id = newsComment.Id,
+                    NewsItemId = newsComment.NewsItemId,
+                    NewsItemTitle = newsComment.NewsItem.Title,
+                    CustomerId = newsComment.CustomerId,
+                    IpAddress = newsComment.IpAddress,
+                    CreatedOn = _dateTimeHelper.ConvertToUserTime(newsComment.CreatedOnUtc, DateTimeKind.Utc),
+                    CommentTitle = newsComment.CommentTitle,
+                    CommentText = HtmlUtils.ConvertPlainTextToHtml(newsComment.CommentText.HtmlEncode()),
+                    CustomerName = customer.GetDisplayName(T)
+                };
+
+                return commentModel;
+            });
+
+            return new JsonResult
+            {
+                Data = model
+            };
+        }
+
+        [GridAction(EnableCustomBinding = true)]
+        [Permission(Permissions.Cms.News.Update)]
+        public ActionResult CommentDelete(int? filterByNewsItemId, int id, GridCommand command)
+        {
+            var comment = _customerContentService.GetCustomerContentById(id) as NewsComment;
+
+            var newsItem = comment.NewsItem;
+            _customerContentService.DeleteCustomerContent(comment);
+
+            _newsService.UpdateCommentTotals(newsItem);
+
+            return Comments(filterByNewsItemId, command);
+        }
+
+        #endregion
+    }
+}
